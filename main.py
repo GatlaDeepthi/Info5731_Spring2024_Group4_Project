7653fc068a22f4907e26fbf41949eb37d89b7783
GatlaDeepthi <158584661+GatlaDeepthi@users.noreply.github.com>
2024-05-05 13:12:27 -0500

    Update main.py
    Joining the data

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+from Libraries and keys import *
+from Aspect_Extraction import *
+from Embedding import *
+from Vector_Database_Creation import *
+from Review_Retrieval import *
+from LLM import *
 
